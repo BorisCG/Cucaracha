9d0f50cd3e43b50ef84c7457877d008991e80357
BorisCG <38569282+BorisCG@users.noreply.github.com>
2018-04-25 15:21:07 -0300

    Ahora el codigo esta identado :)

--- battlebotprototype.ino
+++ battlebotprototype.ino
@@ -1,56 +1,56 @@
-char nombre[10]= "Garrapata";
-char password[10]="1234";
-char baud='4';
-int a=13;
-int b=14;
-int c=15;
-int d=16;
+char nombre[10] = "Garrapata";
+char password[10] = "1234";
+char baud = '4';
+int a = 13;
+int b = 14;
+int c = 15;
+int d = 16;
 
-void setup() 
+void setup()
 {
-Serial.begin(9600);
-pinMode(a,OUTPUT);
-pinMode(b,OUTPUT);
-pinMode(c,OUTPUT);
-pinMode(d,OUTPUT);
+  Serial.begin(9600);
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
 }
 
 void loop() {
-if(Serial.available()>=1)
-{
-char entrada=Serial.read();
+  if (Serial.available() >= 1)
+  {
+    char entrada = Serial.read();
 
-if (entrada=='F')
-{
-digitalWrite(a,HIGH);
-digitalWrite(b,LOW);
-digitalWrite(c,HIGH);
-digitalWrite(d,LOW);
-Serial.println("Adelante");
-}
-else if(entrada=='B')
-{
-digitalWrite(a,LOW);
-digitalWrite(b,HIGH);
-digitalWrite(c,LOW);
-digitalWrite(d,HIGH);
-Serial.println("Atras");
-}
-else if(entrada=='R')
-{
-digitalWrite(a,HIGH);
-digitalWrite(b,LOW);
-digitalWrite(c,LOW);
-digitalWrite(d,HIGH);
-Serial.println("Derecha");
-}
-else if(entrada=='L')
-{
-digitalWrite(a,LOW);
-digitalWrite(b,HIGH);
-digitalWrite(c,HIGH);
-digitalWrite(d,LOW);
-Serial.println("Izquierda");
-}
-}
+    if (entrada == 'F')
+    {
+      digitalWrite(a, HIGH);
+      digitalWrite(b, LOW);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      Serial.println("Adelante");
+    }
+    else if (entrada == 'B')
+    {
+      digitalWrite(a, LOW);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, LOW);
+      digitalWrite(d, HIGH);
+      Serial.println("Atras");
+    }
+    else if (entrada == 'R')
+    {
+      digitalWrite(a, HIGH);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, HIGH);
+      Serial.println("Derecha");
+    }
+    else if (entrada == 'L')
+    {
+      digitalWrite(a, LOW);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      Serial.println("Izquierda");
+    }
+  }
 }
